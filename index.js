1612944ce330697a3a76a11064d6ef1624187457
Dawna Backmeier <karanacesin@yahoo.com>
2020-07-10 02:37:54 -0400

    added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const server = require("./server.js");
 
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {
     console.log(`Server Running on http://localhost:${PORT}`)
 })
